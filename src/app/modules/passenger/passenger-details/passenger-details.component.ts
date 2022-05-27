1551c958c17ffbe466657bb046827c86d2403659
Louka Gamal <loukagamal49@gmail.com>
2022-05-27 16:26:45 +0200

    form valid

--- src/app/modules/passenger/passenger-details/passenger-details.component.ts
+++ src/app/modules/passenger/passenger-details/passenger-details.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { AvailableFlights } from '../../home/home/models/availabelFlights';
 import { TiketsService } from '../../home/services/tikets.service';
 import { passengerDetails } from '../models/passengerDetails';
@@ -13,12 +14,12 @@ import { PassengerService } from '../services/passenger.service';
 export class PassengerDetailsComponent implements OnInit {
   passengerForm: FormGroup = new FormGroup({});
   passengerdata!: passengerDetails;
-  tiketBooked!:AvailableFlights;
- tiket=false;
+  tiketBooked!: AvailableFlights;
+  tiket = false;
 
 
 
-  constructor(private fb: FormBuilder, private passengerervice: PassengerService ,private tikets: TiketsService) { this.initForm() }
+  constructor(private fb: FormBuilder, private passengerervice: PassengerService, private tikets: TiketsService ,private routet:Router) { this.initForm() }
 
   ngOnInit(): void {
     this.getTiket();
@@ -29,30 +30,34 @@ export class PassengerDetailsComponent implements OnInit {
       firstName: ['', [Validators.required]],
       lastName: ['', [Validators.required]],
       email: ['', [Validators.email]],
-      phone: ['', [Validators.minLength(11), Validators.maxLength(11), Validators.pattern('/^[0-9-]+$/')]],
+      phone: ['', [Validators.minLength(11), Validators.maxLength(12), Validators.pattern("^((\\+02-?)|0)?[0-9]{10}$")]],
       gender: ['', Validators.required],
       code: ['', Validators.required],
       payments: ['', Validators.required],
       cardNumber: ['', Validators.required],
       expired: ['', Validators.required],
-      cvv: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(3)]]
+      cvv: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(4)]]
 
     })
   }
   onSubmit() {
     this.passengerdata = this.passengerForm.value
-    this.passengerervice.setPassenger(this.passengerdata)
-    console.log(this.passengerForm.value);
+console.log( this.passengerForm.value);
+
+    if (this.passengerForm.valid)
+     { this.passengerervice.setPassenger(this.passengerdata)
+      this.routet.navigate(['home'])
+    } 
+    else {window.alert("Pless Complete the data"); }
 
 
   }
-  getTiket(){
-    this.tiketBooked= this.tikets.getTikets() ;
-    if(this.tiketBooked)
-    { return true;}
-    else{ return false;}
-   
+  getTiket() {
+    this.tiketBooked = this.tikets.getTikets();
+    if (this.tiketBooked) { return true; }
+    else { return false; }
+
   }
- 
+
 
 }
